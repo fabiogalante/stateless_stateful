51b9ccb5e7b17e9e0a13215a91c6154831a3fb64
Fabio Galante Manse <fabiogalantemans@gmail.com>
2022-09-10 18:55:49 -0300

    addPostFrameCallback

--- lib/statefull.dart
+++ lib/statefull.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:stateless_stateful/stateless.dart';
 
 class HomeStatefull extends StatefulWidget {
+  HomeStatefull() {
+    print('1 - HomeStatefull construtctor');
+  }
+
   //Nao tem o build e sim o create stete
 
   @override
@@ -10,12 +15,30 @@ class HomeStatefull extends StatefulWidget {
 }
 
 class _HomePageStateFullState extends State<HomeStatefull> {
+  _HomePageStateFullState() {
+    print('2 - HomeStatefull construtctor in _HomePageStateFullState');
+  }
+
   String mensagem = 'Mensagem inicial';
 
   @override
   void initState() {
     super.initState();
     mensagem = 'Passou pelo initState';
+    print('3 - initState');
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      print('addPostFrameCallback');
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => HomeStateless(),
+      ));
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print('4 - didChangeDependencies');
   }
 
   @override
@@ -23,19 +46,21 @@ class _HomePageStateFullState extends State<HomeStatefull> {
     //agora um build
     print('buildando o HomePageStateful');
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Text(mensagem),
-          ElevatedButton(
-              onPressed: () {
-                //sem o setState não funciona
-                setState(() {
-                  mensagem = 'Alterei o texto';
-                });
-              },
-              child: const Text('Alterar Texto'))
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(mensagem),
+            ElevatedButton(
+                onPressed: () {
+                  //sem o setState não funciona
+                  setState(() {
+                    mensagem = 'Alterei o texto';
+                  });
+                },
+                child: const Text('Alterar Texto'))
+          ],
+        ),
       ),
     );
   }
